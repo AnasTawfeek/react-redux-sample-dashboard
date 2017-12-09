45814245faa8667608f30a4b3110876ff07a6a51
Anas Tawfeek <anas.tawfeek@outlook.com>
2017-12-09 15:50:52 +0200

    Configure React-Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Users from './pages/Users';
+import Todos from './pages/Todos';
+import Meetings from './pages/Meetings';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Link to="/">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </Link>
+                    <h1 className="App-title">Welcome to React</h1>
+                    <Link to="/users">Users</Link>
+                    <Link to="/todos">Todos</Link>
+                    <Link to="/meetings">Meetings</Link>
+                </header>
+                <div className="App-container">
+                    <Route path="/" exact component={Home} />
+                    <Route path="/users" component={Users} />
+                    <Route path="/todos" component={Todos} />
+                    <Route path="/meetings" component={Meetings} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
